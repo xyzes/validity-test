4bbabc064b9f3a6b7189cca2d85da633b8891526
Esteban D. Espinoza <es.da.espinoza@gmail.com>
2019-10-20 19:57:11 -0400

    Helpers for String comparison
    Added support for Apache implementations of Levenshtein Distance & Double Metaphone

--- src/main/java/com/validity/duplicates/DuplicatesApplicationController.java
+++ src/main/java/com/validity/duplicates/DuplicatesApplicationController.java
@@ -1,6 +1,5 @@
 package com.validity.duplicates;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/test/java/com/validity/duplicates/DuplicatesApplicationTests.java
+++ src/test/java/com/validity/duplicates/DuplicatesApplicationTests.java
@@ -1,5 +1,46 @@
 package com.validity.duplicates;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.apache.commons.codec.language.DoubleMetaphone;
+
+
 class DuplicatesApplicationTests {
 
+    @Test
+    void testLevenshtein(){
+        LevenshteinDistance testLevenshtein = new LevenshteinDistance();
+        Assertions.assertEquals(testLevenshtein.apply("", ""), 0);
+        Assertions.assertEquals(testLevenshtein.apply("", "a"), 1);
+        Assertions.assertEquals(testLevenshtein.apply("boat", "goat"), 1);
+        Assertions.assertEquals(testLevenshtein.apply("flaw", "lawn"), 2);
+        Assertions.assertEquals(testLevenshtein.apply("kitten", "sitting"), 3);
+        Assertions.assertEquals(testLevenshtein.apply("intention", "execution"), 5);
+    }
+
+    @Test
+    void testMetaphone(){
+        DoubleMetaphone testMetaphone = new DoubleMetaphone();
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("John", "john"),
+            true);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("type", "type"),
+            true);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("judge", "juge"),
+            true);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("knock", "nock"),
+            true);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("white", "wite"),
+            true);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("pair", "pear"),
+            true);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("iPhone11", "iPhone"),
+            true);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("The End", "The End."),
+            true);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("bookkeeper", "book keeper"),
+            false);
+        Assertions.assertEquals(testMetaphone.isDoubleMetaphoneEqual("a elephant", "an elephant"),
+            false);
+    }
 }
