493676ef2a9508f7b9265cb4d404ae4a400d9109
Esteban D. Espinoza <es.da.espinoza@gmail.com>
2019-10-20 22:34:57 -0400

    Support for CSV read
    Program uses CSVParser to fetch data and print all lines.

--- src/main/java/com/validity/duplicates/Utils.java
+++ src/main/java/com/validity/duplicates/Utils.java
@@ -1,6 +1,39 @@
 package com.validity.duplicates;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.codec.language.DoubleMetaphone;
+import org.apache.commons.text.similarity.LevenshteinDistance;
 
 public class Utils {
+
+  // String dupes stores entries that are potential duplicates
+  public String dupes = "<li>Test</li><li>Test</li>";
+  // String clear stores entries that are not potential duplicates
+  public String clear = "<li>Test</li><li>Test</li>";
+
+  public void findDuplicates() {
+    try {
+      Reader in = new FileReader("./data/normal.csv");
+      Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+      for (CSVRecord record : records) {
+        System.out.println(record);
+      }
+    }
+    catch(FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
 }
