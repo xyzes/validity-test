c644aee15e2740d4cc25a997faa3b13ab8694514
Esteban D. Espinoza <es.da.espinoza@gmail.com>
2019-10-20 21:24:48 -0400

    Style changes, completed Controller class
    Added JavaDocs and comments, added call to Utils.findDuplicates in DuplicatesApplicationController

--- src/main/java/com/validity/duplicates/DuplicatesApplicationController.java
+++ src/main/java/com/validity/duplicates/DuplicatesApplicationController.java
@@ -3,11 +3,19 @@ package com.validity.duplicates;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The DuplicatesApplicationController is in charge of issuing commands to
+ * obtain or analyze data and returning the output to the web application.
+ */
 @RestController
 public class DuplicatesApplicationController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello, world!";
+        // Stores variables containing duplicates & non-duplicates
+        Utils data = new Utils();
+        data.findDuplicates(); // <--- Calls Utils.findDuplicates
+        return "<p>Potential duplicates</p><ul>" + data.dupes +
+            "</ul><p>Non-duplicates</p><ul>" + data.clear + "</ul>";
     }
 }
--- src/main/java/com/validity/duplicates/Utils.java
+++ src/main/java/com/validity/duplicates/Utils.java
@@ -1,6 +1,24 @@
 package com.validity.duplicates;
 
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.codec.language.DoubleMetaphone;
+import org.apache.commons.text.similarity.LevenshteinDistance;
 
+/**
+ * Utils class contains function to find duplicates in a CSV file and variables
+ * to store duplicates and non-duplicates as HTML list items. These variables are
+ * later accessed by DuplicatesApplicationController.
+ */
 public class Utils {
+
+  // String dupes stores entries that are potential duplicates
+  public String dupes = "<li>Test</li><li>Test</li>";
+  // String clear stores entries that are not potential duplicates
+  public String clear = "<li>Test</li><li>Test</li>";
+
+  public void findDuplicates() {
+    // TODO : Parse through CSV file and use String similarity helpers to find dupes.
+  }
+
+
 }
