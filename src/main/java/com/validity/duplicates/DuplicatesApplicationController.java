a6ffe4acb5ff9939a915c27369497a94c8d7e49a
Esteban D. Espinoza <es.da.espinoza@gmail.com>
2019-10-20 21:24:48 -0400

    Revert "Support for CSV read"
    This reverts commit 44305ea33c0f220b3f5094fe80c71c079f8746da.

--- src/main/java/com/validity/duplicates/DuplicatesApplicationController.java
+++ src/main/java/com/validity/duplicates/DuplicatesApplicationController.java
@@ -3,19 +3,11 @@ package com.validity.duplicates;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * The DuplicatesApplicationController is in charge of issuing commands to
- * obtain or analyze data and returning the output to the web application.
- */
 @RestController
 public class DuplicatesApplicationController {
 
     @RequestMapping("/")
     public String index() {
-        // Stores variables containing duplicates & non-duplicates
-        Utils data = new Utils();
-        data.findDuplicates(); // <--- Calls Utils.findDuplicates
-        return "<p>Potential duplicates</p><ul>" + data.dupes +
-            "</ul><p>Non-duplicates</p><ul>" + data.clear + "</ul>";
+        return "Hello, world!";
     }
 }
