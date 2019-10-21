44305ea33c0f220b3f5094fe80c71c079f8746da
Esteban D. Espinoza <es.da.espinoza@gmail.com>
2019-10-20 21:24:48 -0400

    Support for CSV read
    Program uses CSVParser to fetch data and print all lines.

--- src/main/java/com/validity/duplicates/DuplicatesApplication.java
+++ src/main/java/com/validity/duplicates/DuplicatesApplication.java
@@ -3,10 +3,19 @@ package com.validity.duplicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The DuplicatesApplication class contains the main function and starts
+ * the Spring Boot web application to display duplicates.
+ */
 @SpringBootApplication
 public class DuplicatesApplication {
 
+	/**
+	 * MAIN function. This web application will display duplicates or near-duplicates
+	 * from a contact list.
+	 */
 	public static void main(String[] args) {
+		// Run Spring Boot application
 		SpringApplication.run(DuplicatesApplication.class, args);
 	}
 
